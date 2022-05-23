cab935f4c77780e3dc1fbe7ca9d577b0846d2be0
Melvin <melvin.hernandez@pixtig.com>
2022-05-23 10:43:17 -0600

    update

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express')
-const palindromo = require('./palindromo')
+const palindromo = require('./filesData')
 const app = express()
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  // res.header("Content-Type", "application/json");
+  next()
+})
 
-// Palindromo
-app.get('/iecho', (req, res) => {
-  res.json(palindromo.get(req.query.text))
+// Data de archivos
+app.get('/files/data', async (req, res) => {
+  let flag = true
+  res.header('Content-Type', 'application/json')
+
+  if (req.query.fileName !== undefined) {
+    if (req.query.fileName.length > 0) {
+      flag = false
+      res.json(await palindromo.getFileInfo(req.query.fileName))
+    }
+  }
+
+  if (flag){
+    res.json(await palindromo.getDataFiles())
+  }
+})
+
+// Lista de archivos
+app.get('/files/list', async (req, res) => {
+  res.header('Content-Type', 'application/json')
+  res.json(await palindromo.getFiles())
 })
 
 app.listen(3000, () => console.log('Ready'))
